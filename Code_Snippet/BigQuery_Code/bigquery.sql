b134bda92b027e0cacdff0ca475f38981fb4962a
Samar Santosh Patil <samar.patil5@mail.dcu.ie>
2023-12-20 01:18:33 +0000

    Update bigquery.sql

--- Code_Snippet/BigQuery_Code/bigquery.sql
+++ Code_Snippet/BigQuery_Code/bigquery.sql
@@ -1,1 +1,12 @@
 
+
+#Data Validation after creating tables.
+SELECT distinct Stock_Name FROM `eternal-byte-408521.ALL.Stockcombined` LIMIT 100;
+
+SELECT DISTINCT Stock_name,COUNT(Stock_name) FROM `eternal-byte-408521.predict.linearregression` GROUP BY Stock_name LIMIT 100;
+
+SELECT min(Date), max(Date), count(*) FROM `eternal-byte-408521.Tesla.Tstock`   LIMIT 10;
+
+SELECT * FROM `eternal-byte-408521.Google.Gstock` where open is null and close is null; #no nulls
+
+SELECT * FROM `eternal-byte-408521.Apple.stock` where date is null or high is null or low is null; #no nulls
